37dce28a8e317aed5fdb807af0921b70283894fb
R. LaRoi <are@raewhoo.com>
2023-07-12 15:44:00 +0200

    Updated Project Component

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -14,10 +14,13 @@ export default function Projects() {
   <div className='pro-header'>
     projects
     <div className='p-title'>
-  REACT | JAVASCRIPT | HTML | CSS
+
   <p className='text'>
   Interested in building user interfaces that integrate new technologies and design patterns and committed to creating projects that will engage and provide new perspectives.
+   <div>
+    <a href="https://github.com/R-LaRoi/" target= "_blank" alt="no alternative text"><i className="fa-brands fa-github" id='i-slide'></i></a> </div>
 </p>
+
 </div>
 
   </div>
